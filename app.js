ef475c6fd70e4be583bf862ade7ee9a9ede43026
“LucioDangelo” <ldangelo@itba.edu.ar>
2021-11-13 21:20:34 -0300

    update3

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 const puerto = 3000
 
 app.get("/", (req , res)=>{
 res.sendFile(__dirname + "/views/index.html")
 }); 
-
-app.listen(process.env.PORT || puerto, ()=>{
-console.log('servidor funcionando')
-});
 
 app.get("/login", (req , res)=>{
 res.sendFile(__dirname + "/views/login.html")
@@ -20,3 +16,8 @@ res.sendFile(__dirname + "/views/login.html")
 app.get("/register", (req , res)=>{
 res.sendFile(__dirname + "/views/register.html")
 }); 
+
+app.listen(process.env.PORT || puerto, ()=>{
+console.log("servidor funcionando")
+});
+
