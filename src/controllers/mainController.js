602897ee422bdf7d1d65db6a446b5c42b742e7a3
LucioDangelo <ldangelo@itba.edu.ar>
2021-11-28 21:47:00 +0100

    carpetas organizadas, controler y router index

---
+++ src/controllers/mainController.js
@@ -0,0 +1,42 @@
+const juegosDeMesa = [
+    {
+        id:1,
+        titulo: 'Alien: El juego oficial',
+        descripcionCorta: 'Juego de mesa de Alien',
+        descripcionDetallada: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat rem possimus aut natus, vitae at quia quaerat nam quo error, atque aliquid, velit molestias. Officia harum voluptates quam quis doloremque!',
+        precio: '$5.550',
+        img: 'juegoAlien.jpg'
+    },
+    {
+        id:2,
+        titulo: 'The Walking Dead',
+        descripcionCorta: 'Juego de mesa de The Walking Dead',
+        descripcionDetallada: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat rem possimus aut natus, vitae at quia quaerat nam quo error, atque aliquid, velit molestias. Officia harum voluptates quam quis doloremque!',
+        precio: '$7.500',
+        img: 'juegoTheWalkingDead.jpg'
+    },
+    {
+        id:3,
+        titulo: 'Monopoly: Game of Thrones',
+        descripcionCorta: 'Juego de mesa de Monopoly Game of Thrones',
+        descripcionDetallada: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat rem possimus aut natus, vitae at quia quaerat nam quo error, atque aliquid, velit molestias. Officia harum voluptates quam quis doloremque!',
+        precio: '$8.999',
+        img: 'juegoMonoGOT.jpg'
+    },
+    {
+        id:4,
+        titulo: 'Game of Thrones',
+        descripcionCorta: 'Juego de mesa de Game of Thrones',
+        descripcionDetallada: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat rem possimus aut natus, vitae at quia quaerat nam quo error, atque aliquid, velit molestias. Officia harum voluptates quam quis doloremque!',
+        precio: '$8.999',
+        img: 'juegoGOT.jpg'
+    }
+]
+
+const mainController = {
+    index: (req, res) => {
+        res.render('index', { productos: juegosDeMesa})
+    },
+};
+
+module.exports = mainController;
