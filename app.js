f9e507972e5d79d7ac1eb39ff929dffb687c0768
LucioDangelo <ldangelo@itba.edu.ar>
2022-03-03 14:43:35 -0300

    procfile

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.listen(3000, ()=>{
-    console.log("servidor funcionando")
-    });
+// app.listen(3000, ()=>{
+//     console.log("servidor funcionando")
+//     });
     
+app.listen(process.env.PORT || 3000, function() {console.log('Servidor corriendo en el puerto 3000');
+})
 
 app.get("/", (req , res)=>{
 res.sendFile(__dirname + "/views/index.html")
