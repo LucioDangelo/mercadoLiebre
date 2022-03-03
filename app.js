fef0c3cc8fa0ba21da5a9d64488af8dfe61f84e1
LucioDangelo <ldangelo@itba.edu.ar>
2022-03-03 14:56:54 -0300

    Error

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
+const puerto = 3000
 
-// app.listen(3000, ()=>{
-//     console.log("servidor funcionando")
-//     });
-    
-app.listen(process.env.PORT || 3000, function() {console.log('Servidor corriendo en el puerto 3000');
+app.listen(process.env.PORT || puerto, () => {
+    console.log("Servidor corriendo en el puerto " + puerto);
 });
 
 app.get("/", (req , res)=>{
