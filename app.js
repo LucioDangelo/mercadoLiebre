6f23120920746ed06423b2744a793640e5097e5f
LucioDangelo <ldangelo@itba.edu.ar>
2022-03-03 14:50:26 -0300

    prueba

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static("public"));
 //     });
     
 app.listen(process.env.PORT || 3000, function() {console.log('Servidor corriendo en el puerto 3000');
-})
+});
 
 app.get("/", (req , res)=>{
 res.sendFile(__dirname + "/views/index.html")
